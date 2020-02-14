7455cd24df775bb9b423dd80b7efacf8cb13a88e
Brendan Asselstine <mail.asselstine@gmail.com>
2020-02-13 23:50:31 -0700

    Step 3: Process Events

--- src/mapping.ts
+++ src/mapping.ts
@@ -20,82 +20,9 @@ import {
   SponsorshipDeposited,
   Withdrawn
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { PoolPrize } from '../generated/schema'
 
-export function handleAdminAdded(event: AdminAdded): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.admin = event.params.admin
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.accountedBalance(...)
-  // - contract.balance(...)
-  // - contract.balanceOf(...)
-  // - contract.cToken(...)
-  // - contract.calculateWinner(...)
-  // - contract.canLock(...)
-  // - contract.committedBalanceOf(...)
-  // - contract.committedSupply(...)
-  // - contract.cooldownDuration(...)
-  // - contract.cooldownEndAt(...)
-  // - contract.currentCommittedDrawId(...)
-  // - contract.currentOpenDrawId(...)
-  // - contract.daiToken(...)
-  // - contract.estimatedInterestRate(...)
-  // - contract.getDraw(...)
-  // - contract.isAdmin(...)
-  // - contract.isLocked(...)
-  // - contract.lockDuration(...)
-  // - contract.lockEndAt(...)
-  // - contract.moveCommitted(...)
-  // - contract.nextFeeBeneficiary(...)
-  // - contract.nextFeeFraction(...)
-  // - contract.openBalanceOf(...)
-  // - contract.openSupply(...)
-  // - contract.paused(...)
-  // - contract.poolToken(...)
-  // - contract.saiPool(...)
-  // - contract.saiToken(...)
-  // - contract.scdMcdMigration(...)
-  // - contract.sponsorshipAndFeeBalanceOf(...)
-  // - contract.supplyRatePerBlock(...)
-  // - contract.token(...)
-  // - contract.totalBalanceOf(...)
-  // - contract.withdrawCommittedDeposit(...)
-  // - contract.withdrawCommittedDepositFrom(...)
-}
+export function handleAdminAdded(event: AdminAdded): void {}
 
 export function handleAdminRemoved(event: AdminRemoved): void {}
 
@@ -105,7 +32,15 @@ export function handleCommittedDepositWithdrawn(
   event: CommittedDepositWithdrawn
 ): void {}
 
-export function handleDeposited(event: Deposited): void {}
+export function handleDeposited(event: Deposited): void { 
+  let contract = Contract.bind(event.address)
+  let drawId = contract.currentOpenDrawId()
+
+  let prize = PoolPrize.load(drawId.toHexString())
+  prize.depositCount = prize.depositCount.plus(BigInt.fromI32(1))
+  prize.depositAmount = prize.depositAmount.plus(event.params.amount)
+  prize.save()
+}
 
 export function handleDepositedAndCommitted(
   event: DepositedAndCommitted
@@ -127,7 +62,15 @@ export function handleNextFeeFractionChanged(
 
 export function handleOpenDepositWithdrawn(event: OpenDepositWithdrawn): void {}
 
-export function handleOpened(event: Opened): void {}
+export function handleOpened(event: Opened): void {
+  let prize = new PoolPrize(event.params.drawId.toHexString())
+  prize.drawId = event.params.drawId
+  prize.depositCount = BigInt.fromI32(0)
+  prize.depositAmount = BigInt.fromI32(0)
+  prize.withdrawalCount = BigInt.fromI32(0)
+  prize.withdrawalAmount = BigInt.fromI32(0)
+  prize.save()
+}
 
 export function handleRewarded(event: Rewarded): void {}
 
@@ -139,4 +82,12 @@ export function handleSponsorshipAndFeesWithdrawn(
 
 export function handleSponsorshipDeposited(event: SponsorshipDeposited): void {}
 
-export function handleWithdrawn(event: Withdrawn): void {}
+export function handleWithdrawn(event: Withdrawn): void {
+  let contract = Contract.bind(event.address)
+  let drawId = contract.currentOpenDrawId()
+
+  let prize = PoolPrize.load(drawId.toHexString())
+  prize.withdrawalCount = prize.withdrawalCount.plus(BigInt.fromI32(1))
+  prize.withdrawalAmount = prize.withdrawalAmount.plus(event.params.amount)
+  prize.save()
+}
